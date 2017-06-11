8231a4fea20f9bae64779587aa2a5809d58b7f71
kolastar2016 <kola_star@meta.ua>
2017-06-11 20:24:31 +0300

    task_13_learn_function_in_class

---
+++ script/lesson_13.js
@@ -0,0 +1,38 @@
+/*
+var x = function(){
+  console.log("hello");
+  
+}
+x();
+
+var fact = function f(n){
+  if (n==0||n==1){
+    return 1;
+  }
+  var k = f(n-1)*n;
+  if (k>Number.MAX_VALUE){
+    return "biges";
+  }
+  return k;
+}
+
+console.log(fact(5));
+console.log(fact(0));
+console.log(fact(7));
+console.log(fact(170));
+console.log(fact(171));
+console.log(fact(100));
+
+
+
+var cos= function(x){
+  return Math.cos(x);
+}
+console.log("next");
+console.log(cos(1));
+
+console.log(sin(0.5));
+function sin(x){
+  return Math.sin(x);
+}
+*/
