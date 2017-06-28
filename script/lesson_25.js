3b43443b639d764a08a1dfc9f33fc4f531236260
kolastar2016 <kola_star@meta.ua>
2017-06-29 00:18:08 +0300

    this_is_END

---
+++ script/lesson_25.js
@@ -0,0 +1,57 @@
+/*
+//alert(window.navigator.userAgent);
+document.write(window.navigator.userAgent);
+
+document. write('<br>'+screen.width+'*'+ screen.height); 
+
+setTimeout(function(){
+  var b1=window.document.createElement('input');
+  document.body.appendChild(b1);
+  b1.type='button';
+  b1.value='Google';
+  b1.onclick=function(){
+    location.assign('https://www.google.com.ua');
+  }
+},4000);
+
+setTimeout(function(){
+  var b2=window.document.createElement('input');
+  document.body.appendChild(b2);
+  b2.type='button';
+  b2.value="obnovit";
+  b2.onclick=function(){
+    location.reload()
+  }
+},2000);
+
+//function getChildNodes(obj){
+  //var coll=obj.childNodes;
+  //var win=window.open('','_blank','width=100,height=100');
+  var str='';
+  //for (var i=0; i<coll.length; i++) { 
+    //win.document.write(coll[i].nodeName+'<br>');
+    ////win.document.write(coll[i].nodeName+coll[i].nodeType+'<br>');
+      if(coll[i].nodeType==3){
+        str=coll[i].data+ '<br>';
+      }
+        else if(coll[i].nodeType==1){
+          str=coll[i].textContent;
+        }
+        win.document.write(coll[i].nodeName+' '+coll.nodeType+' '+str+'<br>');
+  //} 
+//}
+//getChildNodes(document.body);
+//getChildNodes(document.head);
+//getChildNodes(document.documentElement);
+
+
+//function getChildren(obj){
+  //var coll=obj.children;
+  //var win=window.open('','_blank','width=200,height=200');
+  //for (var i=0; i<coll.length; i++) { 
+    //win.document.write(coll[i].tagName+'<br>');
+  //} 
+//}
+//getChildren(document.head);
+//getChildren(document.documentElement);
+*/
